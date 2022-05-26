f06d616591a792d0ff615e9bf74cad6e77a5e0ff
Henrique Guilherme <henrique.guilherme07@outlook.com>
2022-05-26 15:54:47 -0300

    Seeding Service

--- SalesWebsite/Models/Seller.cs
+++ SalesWebsite/Models/Seller.cs
@@ -13,15 +13,17 @@ namespace SalesWebsite.Models
         public  DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public ICollection<SalesRecord> SalesRecords { get; set; }
+        public Department Department { get; set; }
 
         public Seller() { }
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary)
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
+            Department = department;
         }
 
         public void AddSales(SalesRecord sr)
--- SalesWebsite/Startup.cs
+++ SalesWebsite/Startup.cs
@@ -39,14 +39,17 @@ namespace SalesWebsite
             services.AddDbContext<SalesWebsiteContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebsiteContext"),
                     builder => builder.MigrationsAssembly("SalesWebsite")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
