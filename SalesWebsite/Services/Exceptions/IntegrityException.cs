e6a0e45bb9270474c6685ae7f91dc9a0010a7d3c
Henrique Guilherme <henrique.guilherme07@outlook.com>
2022-05-27 15:44:55 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebsite/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalesWebsite.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+    }
+}
