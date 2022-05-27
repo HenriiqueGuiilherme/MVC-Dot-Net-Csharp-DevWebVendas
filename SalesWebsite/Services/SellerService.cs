4424bebb85fc9537cead46de4ceacbf2207e2194
Henrique Guilherme <henrique.guilherme07@outlook.com>
2022-05-27 10:04:58 -0300

    Delete Seller

--- SalesWebsite/Services/SellerService.cs
+++ SalesWebsite/Services/SellerService.cs
@@ -16,6 +16,7 @@ namespace SalesWebsite.Services
             _context = context;
         }
 
+        //Insert
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
@@ -24,6 +25,18 @@ namespace SalesWebsite.Services
         public void Insert(Seller obj)
         {
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        //Remove
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
