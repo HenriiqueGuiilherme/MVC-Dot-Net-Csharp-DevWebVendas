bee2d3b6f79d644d0e7ab42d9a53c792c2959062
Henrique Guilherme <henrique.guilherme07@outlook.com>
2022-05-27 08:17:16 -0300

    Foreign Key not null (referential integrity)

--- SalesWebsite/Models/Seller.cs
+++ SalesWebsite/Models/Seller.cs
@@ -14,6 +14,7 @@ namespace SalesWebsite.Models
         public double BaseSalary { get; set; }
         public ICollection<SalesRecord> SalesRecords { get; set; }
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
 
         public Seller() { }
         public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
