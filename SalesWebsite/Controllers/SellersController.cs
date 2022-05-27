e05a3a5304d9df6fd26ff38a6f8316432d535a9b
Henrique Guilherme <henrique.guilherme07@outlook.com>
2022-05-27 14:01:50 -0300

    Update seller and custom service exception

--- SalesWebsite/Controllers/SellersController.cs
+++ SalesWebsite/Controllers/SellersController.cs
@@ -2,6 +2,7 @@
 using SalesWebsite.Models;
 using SalesWebsite.Models.ViewModels;
 using SalesWebsite.Services;
+using SalesWebsite.Services.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,9 +42,9 @@ namespace SalesWebsite.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        public IActionResult Delete (int? id)
+        public IActionResult Delete(int? id)
         {
-            if(id == null)
+            if (id == null)
             {
                 return NotFound();
             }
@@ -78,6 +79,48 @@ namespace SalesWebsite.Controllers
             }
             return View(obj);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj, Departments = departments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
-    
+
 }
